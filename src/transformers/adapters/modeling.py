e0389f42f4eb534c8fb5b527586ebcf3db81a676
Lawrence0319 <66525267+Lawrence0319@users.noreply.github.com>
2023-08-09 23:32:38 +0800

    Update modeling.py

--- src/transformers/adapters/modeling.py
+++ src/transformers/adapters/modeling.py
@@ -338,21 +338,21 @@ class BertFusion(nn.Module):
 
         if self.config["query"]:
             self.query = nn.Linear(self.dense_size, self.dense_size)
-            self.queryrelu = nn.ReLU()
+            self.queryact = nn.SiLU()
             print("query!")
-            self.queryrelu.apply(Adapter.init_bert_weights)
+            self.queryact.apply(Adapter.init_bert_weights)
 
         if self.config["key"]:
             self.key =  nn.Linear(self.dense_size, self.dense_size)
-            self.keyrelu = nn.ReLU()
+            self.keyact = nn.SiLU()
             print("key!")
-            self.keyrelu.apply(Adapter.init_bert_weights)
+            self.keyact.apply(Adapter.init_bert_weights)
 
         if self.config["value"]:
             self.value =  nn.Linear(self.dense_size, self.dense_size, bias=False)
-            self.valuerelu = nn.ReLU()
+            self.valueact = nn.SiLU()
             print("value!")
-            self.valuerelu.apply(Adapter.init_bert_weights)
+            self.valueact.apply(Adapter.init_bert_weights)
             if self.config["value_initialized"]:
                 self.value.weight.data = (torch.zeros(self.dense_size, self.dense_size) + 0.000001).fill_diagonal_(1.0)
 
@@ -368,18 +368,18 @@ class BertFusion(nn.Module):
             value += residual[:, :, None, :].repeat(1, 1, value.size(2), 1)
 
         if self.config["query"]:
-            query_layer = self.queryrelu(self.query(query))
+            query_layer = self.queryact(self.query(query))
         else:
             query_layer = query
 
         if self.config["key"]:
-            key_layer = self.keyrelu(self.key(key))
+            key_layer = self.keyact(self.key(key))
         else:
             key_layer = key
 
         if self.config["value"] and self.config["value_before_softmax"]:
             # key/value have dims => batch, toks, number-of-adapters, feats
-            value_layer = self.valuerelu(self.value(value))
+            value_layer = self.valueact(self.value(value))
         else:
             value_layer = value
 
