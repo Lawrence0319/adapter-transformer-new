3f198a0dd18a395d9a15ab1b42d0749de0f63a13
Lawrence0319 <66525267+Lawrence0319@users.noreply.github.com>
2023-08-12 02:37:24 +0800

    Update modeling.py

--- src/transformers/adapters/modeling.py
+++ src/transformers/adapters/modeling.py
@@ -338,19 +338,19 @@ class BertFusion(nn.Module):
 
         if self.config["query"]:
             self.query = nn.Linear(self.dense_size, self.dense_size)
-            self.queryact = nn.LeakyReLU()
+            self.queryact = nn.Sigmoid()
             print("query!")
             self.queryact.apply(Adapter.init_bert_weights)
 
         if self.config["key"]:
             self.key =  nn.Linear(self.dense_size, self.dense_size)
-            self.keyact = nn.LeakyReLU()
+            self.keyact = nn.Sigmoid()
             print("key!")
             self.keyact.apply(Adapter.init_bert_weights)
 
         if self.config["value"]:
             self.value =  nn.Linear(self.dense_size, self.dense_size, bias=False)
-            self.valueact = nn.LeakyReLU()
+            self.valueact = nn.Sigmoid()
             print("value!")
             self.valueact.apply(Adapter.init_bert_weights)
             if self.config["value_initialized"]:
